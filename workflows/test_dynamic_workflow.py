ea845a8ce5b4ff0f3231916e42e7974d09ec31e5
Clemente Cuevas <ccuevas@pryoninc.com>
2023-12-27 18:55:59 -0800

    use eager workflow

--- workflows/test_dynamic_workflow.py
+++ workflows/test_dynamic_workflow.py
@@ -1,8 +1,10 @@
 from typing import Set, List, Tuple
+import asyncio
 from flytekit.types.directory import FlyteDirectory
 from io import BytesIO
 from flytekit.types.file import FlyteFile
 from flytekit import task, dynamic, LaunchPlan, workflow, Resources, map_task, conditional
+from flytekit.experimental import eager
 from minio import Minio
 import logging
 
@@ -45,12 +47,23 @@ def fill_filesystem(internal: bool = True):
     logger.debug("All done filling 'big-bucket'")
 
     
-    
-
-
-
 @task(requests=Resources(cpu='2', mem='1Gi'))
-def paginate_bucket(bucket_name: str, internal: bool = True) -> (list[str], list[str]):
+def paginate_directory(bucket_name: str, directory: str, internal: bool = True) -> Tuple[List[str], List[str]]:
+    logger.debug(f"Paginating {directory}")
+    minio_client = get_minio_client(internal=internal)
+    objects = set()
+    subpaths = set()
+    path_objects = minio_client.list_objects(bucket_name, prefix=directory)
+    for o in path_objects:
+        if o.is_dir:
+            subpaths.add(o.object_name)
+        else:
+            objects.add(o.object_name)
+    logger.debug(f"{len(objects) + len(subpaths)} objects in directory")
+    return (list(objects), list(subpaths))
+    
+@task(requests=Resources(cpu='1', mem='1Gi'))
+def paginate_bucket(bucket_name: str, internal: bool = True) -> Tuple[List[str], List[str]]:
     logger.debug(f"Paginating bucket {bucket_name}")
     minio_client = get_minio_client(internal=internal)
     objects = set()
@@ -62,32 +75,12 @@ def paginate_bucket(bucket_name: str, internal: bool = True) -> (list[str], list
         else:
             objects.add(o.object_name)
     logger.debug(f"{len(directories) + len(objects)} objects in bucket")
-    logger.debug(f"Finished paginating bucket {bucket_name}")
-    new_objects = list(objects)
-    new_directories = list(directories)
-    return (new_directories, new_objects)
-
-@task(requests=Resources(cpu='2', mem='1Gi'))
-def paginate_directory(bucket_name: str, directory: str, internal: bool = True) -> (list[str], list[str]):
-    logger.debug(f"Paginating directory {directory}")
-    minio_client = get_minio_client(internal=internal)
-    objects = set()
-    subpaths = set()
-    path_objects = minio_client.list_objects(bucket_name, prefix=directory)
-    logger.debug(f"{len(path_objects)} objects in directory")
-    for o in path_objects:
-        if o.is_dir():
-            subpaths.add(o.object_name)
-        else:
-            objects.add(o.object_name)
-    logger.debug(f"Finished paginating directory {directory}")
-    return (list(subpaths), list(objects))
-
+    return (list(directories), list(objects))
     
 
 
-@dynamic
-def task_wise_paginate_through_filesystem(internal: bool = True) -> list[str]:
+@eager
+async def task_wise_paginate_through_filesystem(internal: bool = True) -> List[str]:
     """A dynamic workflow:
     Using the minio_client, get a list of all bucket names
     For each bucket:
@@ -104,25 +97,26 @@ def task_wise_paginate_through_filesystem(internal: bool = True) -> list[str]:
     Returns a set of every object in the bucket without recursively exploring the bucket in a single step
     """
     logger.debug("Filling filesystem with data")
-    fill_filesystem(internal=internal)
+    await fill_filesystem(internal=internal)
     logger.debug("Done filling filesystem")
-    object_paths = set()
+    object_paths = {}
     # this is defined in tests, there are other buckets that get used by 
     # flyte, so let's just use the one we create
     bucket_name = "big-bucket"
     logger.debug("going into paginate bucket")
-    directories, objects = paginate_bucket(internal=internal, bucket_name=bucket_name)
+    directories, objects = await paginate_bucket(internal=internal, bucket_name=bucket_name)
+    logger.debug("Done paginating big bucket")
     for o in objects:
-        object_paths.add(o)
+        object_paths[o] = True
     for directory in directories:
-        subdirectories, objects = paginate_directory(bucket_name=bucket_name, directory=directory, internal=internal)
-        object_paths.update(set(objects))
+        subdirectories, new_objects = await paginate_directory(internal=internal, bucket_name=bucket_name, directory=directory)
+        for o in new_objects:
+            object_paths[o] = True
         while subdirectories:
-            subdirectories, objects =paginate_directory(bucket_name=bucket_name, directory=directory, internal=internal)
-            for o in objects:
-                object_paths.add(o)
-    logger.debug("All object paths listed")
-    return list(set(object_paths))
+            subdirectories, new_objects = await paginate_directory(internal=internal, bucket_name=bucket_name, directory=subdirectories)
+            for o in new_objects:
+                object_paths[o] = True
+    return list(object_paths.keys())
 
 @workflow
 def dynamic_task_wise_paginate_through_filesystem(internal: bool = True) -> List[str]:
@@ -130,9 +124,4 @@ def dynamic_task_wise_paginate_through_filesystem(internal: bool = True) -> List
     return task_wise_paginate_through_filesystem(internal=internal)
 
 if __name__ == '__main__':
-    standard_scale_launch_plan = LaunchPlan.get_or_create(
-        dynamic_task_wise_paginate_through_filesystem,
-        name="paginate_through_fs",
-        default_inputs={"internal": False}
-    )
-    standard_scale_launch_plan(internal=False)
+    asyncio.run(task_wise_paginate_through_filesystem(internal=False))
